c88aac99a9d574b973f0d678ba37bc67c7603f60
Tahmid77 <19-41077-2@student.aiub.edu>
2023-10-03 22:32:33 +1100

    Student Class

--- Assignment-2/AssignmentStatistics.java
+++ Assignment-2/AssignmentStatistics.java
@@ -1,11 +1,47 @@
 import java.io.*;
 import java.util.*;
-/**
- * Write a description of class AssignmentStatistics here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
+class Student {
+    private String firstName;
+    private String lastName;
+    private String studentID;
+    private double[] marks = new double[3];
+    private double totalMark;
+
+    public Student(String firstName, String lastName, String studentID, double[] marks) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentID = studentID;
+        this.marks = marks;
+        this.totalMark = calculateTotalMark();
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public double[] getMarks() {
+        return marks;
+    }
+
+    public double getTotalMark() {
+        return totalMark;
+    }
+
+    private double calculateTotalMark() {
+        double total = 0.0;
+        for (double mark : marks) {
+            total += mark;
+        }
+        return total;
+    }
+}
+
+
 public class AssignmentStatistics {
     
     
